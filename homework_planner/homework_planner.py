8404509eef142d3ad070e8a070433d855c903c0e
Andrew Magerman <andrew.magerman@magerman.com>
2021-03-15 23:08:28 +0100

    next step on nice display for homework

--- homework_planner/homework_planner.py
+++ homework_planner/homework_planner.py
@@ -1,3 +1,4 @@
+import pprint
 from pathlib import Path
 from typing import List
 
@@ -13,10 +14,43 @@ def planning_dict():
 def write_plan():
     pass
 
+class Reference(object):
+    def __init__(self, dictie):
+        if 'Reading_extra' in dictie:
+            self.title, self.path = dictie['Reading_extra']['title'], \
+                          dictie['Reading_extra'][
+                              'path']
+    def url(self):
+        return 'hello'
 
-def homework_this_week(param: dict) -> List[str]:
-    return ['hello']
 
+def homework_this_week(value: dict) -> List[str]:
+    week = value["week"]
+
+    if "book_exercises" in value:
+        homework_line_extra = f' including book exercises {value["book_exercises"]}'
+    else:
+        homework_line_extra = ""
+
+    return [f'do labs for week {week}',
+            f'read the book {value["Reading"]}',
+            f'watch the lectures {value["Lectures"]}',
+            f'read the course notes for week {week}',
+            f'do homework for week {week}{homework_line_extra}'
+            ]
+
+
+def mickey():
+    all_weeks = planning_dict()
+    for week in all_weeks:
+        print(week)
+        print('\n')
+        for h in homework_this_week(all_weeks[week]):
+            print(h)
+        print('\n')
 
 if __name__ == '__main__':
-    print(planning_dict())
+    pprint.pprint(planning_dict())
+    #a = planning_dict()
+    #print(homework_this_week(a['Week 2']))
+    mickey()
--- homework_planner/test_homework_planner.py
+++ homework_planner/test_homework_planner.py
@@ -1,19 +1,46 @@
+from pathlib import Path
+
 from homework_planner.homework_planner import planning_dict, homework_this_week
 
-a = planning_dict()
+all_weeks = planning_dict()
 
 def test_01():
-    assert a['Week 1']['Theme'] == 'Functional Programming'
+    assert all_weeks['Week 1']['Theme'] == 'Functional Programming'
 
 
 
 
 def test_02():
-    hw_w1 = homework_this_week(a['Week 1'])
-    assert hw_w1[0] == 'ello'
+    hw_w1 = homework_this_week(all_weeks['Week 1'])
+    print(hw_w1)
+    assert hw_w1[0] == "do labs for week 1"
+    assert hw_w1[1] == "read the book Section 1.1, pages 1-31"
+    assert hw_w1[2] == "do homework for week 1"
 
 
+def test_week_2():
+    hw_w2 = homework_this_week(all_weeks['Week 2'])
+    print(all_weeks['Week 2'])
+    assert hw_w2[2] == "do homework for week 2 including book exercises 1.31(a), 1.32(a), 1.33, 1.40, 1.41, 1.43, 1.46"
 
+
+def test_reading_extra():
+    b = all_weeks['Week 7']
+    title, path = b['Reading_extra']['title'], b['Reading_extra']['path']
+    w = Path(__file__).parent.parent
+    print(w)
+    c = w / path
+    print(c.absolute())
+    assert Path(c) == 'hello'
+
+def test_file_exists():
+    for week, dictie in all_weeks.items():
+        if 'Reading_extra' in dictie:
+            title, path = dictie['Reading_extra']['title'], dictie['Reading_extra'][
+                'path']
+            w = Path(__file__).parent.parent
+            c = w / path
+            assert c.exists()
 
 # - read the book pages 1-31
 # - watch lectures 1 & 2
